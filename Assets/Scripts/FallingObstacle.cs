bf18f097a9c43819109686349db1cd74727a608f
Kyumin_Kim <spmuk13@naver.com>
2025-11-25 21:42:06 +0900

    .

---
+++ Assets/Scripts/FallingObstacle.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class FallingObstacle : MonoBehaviour
+{
+    //장애물의 오브젝트 불러옴.
+    // 만약 장애물이 바닥이나 플레이어블에 닿으면 부서짐.
+    //플레이어와 부딪힐시 플레이어에게 부정적 효과 적용
+    //ㄴ플레이어블에게 이벤트 시스템이 필요함
+}
